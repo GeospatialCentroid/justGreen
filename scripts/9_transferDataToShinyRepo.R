8f720464304d090c7b80a37e3274d7802a735573
dune <carver.dan1@gmail.com>
2025-12-03 18:04:25 +0000

    updates rds structure for shinyapp

--- scripts/9_transferDataToShinyRepo.R
+++ scripts/9_transferDataToShinyRepo.R
@@ -1,5 +1,5 @@
 # move all the datasets requires for the application
-pacman::p_load(readr, sf, rmapshaper, tmap)
+pacman::p_load(readr, sf, rmapshaper, tmap, purrr, stringr)
 tmap::tmap_mode("view")
 justGreenData <- "~/trueNAS/work/justGreenShiny/data"
 
@@ -18,11 +18,9 @@ ctm <- read_csv("data/processed/summaryNDVI/allCensusTractsNDVI_2023.csv")
 cities <- sf::st_read("data/processed/top200/top200Cities_vect.gpkg") |>
   rmapshaper::ms_simplify() |>
   sf::st_make_valid()
-qtm(cities)
+# qtm(cities)
 # census tracts
 # gather call census tracts per city?
-index <- 1
-
 grabCT_data <- function(index, cities, ctm) {
   # name
   id <- cities$GEOID[index]
@@ -44,27 +42,67 @@ grabCT_data <- function(index, cities, ctm) {
   return(cityCT)
 }
 
-vals <- purrr::map(
+ct_gpkg <- purrr::map(
   .x = 1:200,
   .f = grabCT_data,
   cities = cities,
   ctm = ctm
 )
-names(vals) <- cities$GEOID
+names(ct_gpkg) <- cities$GEOID
+
+# get the reports for all 200 cities into a list
+file_paths <- list.files(
+  path = "data/products/citySummaries",
+  pattern = "\\.html$",
+  full.names = TRUE
+)
+
+# 2. Read all files into a list
+# This creates a list where every element is a long HTML string
+html_list <- map(file_paths, read_file)
+
+# 3. Create clean "Keys" for the list
+clean_names <- basename(file_paths) %>% # Remove folder path
+  str_remove("^Report-") %>% # Remove "Report-" prefix
+  str_remove("\\.html$") # Remove ".html" suffix
+
+# Assign names to the list
+names(html_list) <- clean_names
+
+# 4. Save as a compressed RDS
+# 'compress = "xz"' is slower to save but creates the smallest file size
+saveRDS(
+  html_list,
+  "data/products/dataForShiny/citySummary.rds",
+  compress = "xz"
+)
+
+
+# generate the rds files  ------------------------------------------------
 
 # store all features in a rds file
-dataBundles <- list(
+healthData <- list(
   cityHealth <- cm,
-  tractHealth <- ctm,
-  cityGPKG <- cities,
-  tractsGPKG <- vals
+  tractHealth <- ctm
 )
+saveRDS(object = healthData, file = "data/products/dataForShiny/healthData.rds")
 
-saveRDS(object = dataBundles, file = "data/products/dataForShiny/data.rds")
+# export the city gpkg
+saveRDS(object = cities, file = "data/products/dataForShiny/citiesGPKG.rds")
+
+# export the census tract data
+saveRDS(object = ct_gpkg, file = "data/products/dataForShiny/tractsGPKG.rds")
+
 
 #copy file to the shiny app folder location
-file.copy(
-  "data/products/dataForShiny/data.rds",
-  justGreenData,
-  overwrite = TRUE
-)
+for (i in list.files(
+  "data/products/dataForShiny",
+  full.names = TRUE
+)) {
+  name <- basename(i)
+  file.copy(
+    from = i,
+    justGreenData,
+    overwrite = TRUE
+  )
+}
